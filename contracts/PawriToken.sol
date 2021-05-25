48e235d4ff9f0c7fd3311569c943cf029c23c977
Aniket Das <aniketdas001@gmail.com>
2021-05-26 00:24:16 +0530

    Third commit

--- contracts/PawriToken.sol
+++ contracts/PawriToken.sol
@@ -7,15 +7,24 @@ contract PawriToken {
     string public name = "pawricoin";
     string public symbol = "PAW";
     string public standard = "pawricoin v1.0";
-    mapping(address => uint256) public balanceOf;
 
+    // defining maps
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // constructor for initialization
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-    // defining events
+    // defining required events
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     // defining functions
     function transfer(address _to, uint256 _value)
@@ -31,6 +40,39 @@ contract PawriToken {
 
         // initializing the transfer event
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // defining allowance
+        allowance[msg.sender][_spender] = _value;
+
+        // initializing approval
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // ensuring transaction is legitimate
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        // adjusting the balances accordingly
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        // initializing the transfer
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/PawriToken.js
+++ test/PawriToken.js
@@ -1,7 +1,7 @@
 var PawriToken = artifacts.require('PawriToken')
 
 contract('PawriToken', function(accounts){
-    it('checks if the contract was initialized with the correct values', function(){
+    it('the contract was initialized with the correct values', function(){
         return PawriToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.name();
@@ -16,7 +16,7 @@ contract('PawriToken', function(accounts){
         });
     });
 
-    it('checks the total supply upon deployment', function(){
+    it('the total supply is initialzed and allocated upon deployment', function(){
         return PawriToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
@@ -28,7 +28,7 @@ contract('PawriToken', function(accounts){
         });
     });
 
-    it('checks if the transfers of token ownership is working as expected', function() {
+    it('the transfers of non-delegated token ownership is working as expected', function() {
         return PawriToken.deployed().then(function(instance) {
           tokenInstance = instance;
           // Test `require` statement first by transferring something larger than the sender's balance
@@ -51,6 +51,67 @@ contract('PawriToken', function(accounts){
           return tokenInstance.balanceOf(accounts[0]);
         }).then(function(balance) {
           assert.equal(balance.toNumber(), 750000, 'did not deduct the amount from the sending account');
+        });
+      });
+
+      it('tokens were accurately approved for delegated transfer', function() {
+        return PawriToken.deployed().then(function(instance) {
+          tokenInstance = instance;
+          return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success) {
+          assert.equal(success, true, 'it returns false');
+          return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+        }).then(function(receipt) {
+          assert.equal(receipt.logs.length, 1, 'does not trigger only one event');
+          assert.equal(receipt.logs[0].event, 'Approval', 'is not the "Approval" event');
+          assert.equal(receipt.logs[0].args._owner, accounts[0], 'does not accurately log the account the tokens are authorized by');
+          assert.equal(receipt.logs[0].args._spender, accounts[1], 'does not accurately log the account the tokens are authorized to');
+          assert.equal(receipt.logs[0].args._value, 100, 'does not accurately log the transfer amount');
+          return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance) {
+          assert.equal(allowance.toNumber(), 100, 'issue in storing the allowance for delegated transfer');
+        });
+      });
+    
+      it('The delegated token transfers were handled as expected', function() {
+        return PawriToken.deployed().then(function(instance) {
+          tokenInstance = instance;
+          fromAccount = accounts[2];
+          toAccount = accounts[3];
+          spendingAccount = accounts[4];
+          // Transfer some tokens to fromAccount
+          return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+        }).then(function(receipt) {
+          // Approve spendingAccount to spend 10 tokens form fromAccount
+          return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+        }).then(function(receipt) {
+          // Try transferring something larger than the sender's balance
+          return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+          assert(error.message.indexOf('revert') >= 0, 'transfers value larger than balance');
+          // Try transferring something larger than the approved amount
+          return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+          assert(error.message.indexOf('revert') >= 0, 'transfers value larger than approved amount');
+          return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(success) {
+          assert.equal(success, true);
+          return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(receipt) {
+          assert.equal(receipt.logs.length, 1, 'does not triggers one event');
+          assert.equal(receipt.logs[0].event, 'Transfer', 'is not the "Transfer" event');
+          assert.equal(receipt.logs[0].args._from, fromAccount, 'does not accurately log the account the tokens are transferred from');
+          assert.equal(receipt.logs[0].args._to, toAccount, 'does not accurately log the account the tokens are transferred to');
+          assert.equal(receipt.logs[0].args._value, 10, 'does not accurately log the transfer amount');
+          return tokenInstance.balanceOf(fromAccount);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 90, 'inaccurately deducts the amount from the sending account');
+          return tokenInstance.balanceOf(toAccount);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 10, 'fails to accurately add the amount from the receiving account');
+          return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance) {
+          assert.equal(allowance.toNumber(), 0, 'issues in deducting the amount from the allowance');
         });
       });
 });
